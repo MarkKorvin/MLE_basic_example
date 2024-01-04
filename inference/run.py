b763f6aa5907c53bdd764a8a68f2944e70e19723
Aleksandr_Morkovin <korvinlesacorp@gmail.com>
2024-01-04 11:38:48 +0200

    Prettification of the inference file

--- inference/run.py
+++ inference/run.py
@@ -1,45 +1,107 @@
-import pandas as pd
-import os
-import sys
-import pickle
-import logging
+"""
+Script loads the latest trained model, data for inference and predicts results.
+Imports necessary packages and modules.
+"""
+
+import argparse
 import json
+import logging
+import os
+import pickle
+import sys
 from datetime import datetime
+from typing import List
+
+import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-
+# Adds the root directory to system path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.abspath(os.path.join(ROOT_DIR, '../data'))
-MODEL_DIR = os.path.abspath(os.path.join(ROOT_DIR, '../models'))
-
 sys.path.append(os.path.dirname(ROOT_DIR))
+
+from utils import get_project_dir, configure_logging
+
+# Loads configuration settings from JSON
 with open("settings.json", "r") as file:
     conf = json.load(file)
 
-def get_latest_model_path():
+# Defines paths
+DATA_DIR = get_project_dir(conf['general']['data_dir'])
+MODEL_DIR = get_project_dir(conf['general']['models_dir'])
+RESULTS_DIR = get_project_dir(conf['general']['results_dir'])
+
+# Initializes parser for command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--infer_file", 
+                    help="Specify inference data file", 
+                    default=conf['inference']['inp_table_name'])
+parser.add_argument("--out_path", 
+                    help="Specify the path to the output table")
+
+
+def get_latest_model_path() -> str:
+    """Gets the path of the latest saved model"""
     latest = None
     for (dirpath, dirnames, filenames) in os.walk(MODEL_DIR):
         for filename in filenames:
-            if not latest or datetime.strptime(latest, "%d.%m.%Y_%H.%M.pickle") < \
-                        datetime.strptime(filename, "%d.%m.%Y_%H.%M.pickle"):
+            if not latest or datetime.strptime(latest, conf['general']['datetime_format'] + '.pickle') < \
+                    datetime.strptime(filename, conf['general']['datetime_format'] + '.pickle'):
                 latest = filename
     return os.path.join(MODEL_DIR, latest)
 
-def get_model(path):
-    with open(path, 'rb') as f:
-        model = pickle.load(f)
-    return model
 
-def get_inference_data(path):
-    df = pd.read_csv(path)
-    return df
-    
+def get_model_by_path(path: str) -> DecisionTreeClassifier:
+    """Loads and returns the specified model"""
+    try:
+        with open(path, 'rb') as f:
+            model = pickle.load(f)
+            logging.info(f'Path of the model: {path}')
+            return model
+    except Exception as e:
+        logging.error(f'An error occurred while loading the model: {e}')
+        sys.exit(1)
+
+
+def get_inference_data(path: str) -> pd.DataFrame:
+    """loads and returns data for inference from the specified csv file"""
+    try:
+        df = pd.read_csv(path)
+        return df
+    except Exception as e:
+        logging.error(f"An error occurred while loading inference data: {e}")
+        sys.exit(1)
+
+
+def predict_results(model: DecisionTreeClassifier, infer_data: pd.DataFrame) -> pd.DataFrame:
+    """Predict de results and join it with the infer_data"""
+    results = model.predict(infer_data)
+    infer_data['results'] = results
+    return infer_data
+
+
+def store_results(results: pd.DataFrame, path: str = None) -> None:
+    """Store the prediction results in 'results' directory with current datetime as a filename"""
+    if not path:
+        if not os.path.exists(RESULTS_DIR):
+            os.makedirs(RESULTS_DIR)
+        path = datetime.now().strftime(conf['general']['datetime_format']) + '.csv'
+        path = os.path.join(RESULTS_DIR, path)
+    pd.DataFrame(results).to_csv(path, index=False)
+    logging.info(f'Results saved to {path}')
+
+
+def main():
+    """Main function"""
+    configure_logging()
+
+    model = get_model_by_path(get_latest_model_path())
+    infer_file = parser.parse_args().infer_file
+    infer_data = get_inference_data(os.path.join(DATA_DIR, infer_file))
+    results = predict_results(model, infer_data)
+    store_results(results, parser.parse_args().out_path)
+
+    logging.info(f'Prediction results: {results}')
+
 
 if __name__ == "__main__":
-    path = get_latest_model_path()
-    print(path)
-    model = get_model(path)
-    df = get_inference_data(os.path.join(DATA_DIR, "xor_inference_data.csv"))
-    res = model.predict(df)
-    print(res)
-    
+    main()
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+import logging
+
 def singleton(class_):
     instances = {}
     def getinstance(*args, **kwargs):
@@ -5,3 +8,12 @@ def singleton(class_):
             instances[class_] = class_(*args, **kwargs)
         return instances[class_]
     return getinstance
+
+def get_project_dir(sub_dir: str) -> str:
+    """Return path to a project subdirectory."""
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), sub_dir))
+
+def configure_logging() -> None:
+    """Configures logging"""
+    logging.basicConfig(level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
