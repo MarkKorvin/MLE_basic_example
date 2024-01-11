f4c413b8ec036e1b5969bb4a79b6953f18060d01
Aleksandr <36667016+MarkKorvin@users.noreply.github.com>
2024-01-11 12:16:15 +0200

    Revert "Added comments and params into data_gen script"

--- data_process/data_generation.py
+++ data_process/data_generation.py
@@ -1,42 +1,27 @@
-"""
-This script generates a XOR data set and saves it for future use in Machine Learning tasks. 
-It allows a separate instance for training (labeled data) and inference (unlabeled data).
-"""
-
-# Importing required libraries
 import numpy as np
 import pandas as pd
 import logging
 import os
 import sys
-import json
 
-# Define directories
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(ROOT_DIR))
-from utils import singleton, get_project_dir, configure_logging
+
+from utils import singleton
 
 DATA_DIR = os.path.abspath(os.path.join(ROOT_DIR, '../data'))
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
-CONF_FILE = os.getenv('CONF_PATH')
 
-# Load configuration settings from JSON
-with open(CONF_FILE, "r") as file:
-    conf = json.load(file)
+TRAIN_PATH = os.path.join(DATA_DIR, 'xor_train_data.csv')
+INFERENCE_PATH = os.path.join(DATA_DIR, 'xor_inference_data.csv')
 
-# Define paths
-DATA_DIR = get_project_dir(conf['general']['data_dir'])
-TRAIN_PATH = os.path.join(DATA_DIR, conf['train']['table_name'])
-INFERENCE_PATH = os.path.join(DATA_DIR, conf['inference']['inp_table_name'])
 
-# Singleton class for generating XOR data set
 @singleton
 class XorSetGenerator():
     def __init__(self):
         self.df = None
-
-    # Method to create the XOR data
+    
     def create(self, len: int, save_path: os.path, is_labeled: bool = True):
         self.df = self._generate_features(len)
         if is_labeled:
@@ -45,26 +30,20 @@ class XorSetGenerator():
             self.save(self.df, save_path)
         return self.df
 
-    # Method to generate features
     def _generate_features(self, n: int) -> pd.DataFrame:
         x1 = np.random.choice([True, False], size=n)
-        x2 = np.random.choice([True, False], size=n)
-        return pd.DataFrame(list(zip(x1, x2)), columns=['x1', 'x2'])
+        x2 = np.random.choice([True, False], size=n) 
+        return pd.DataFrame(list(zip(x1, x2)), columns = ['x1', 'x2']) 
 
-    # Method to generate target
     def _generate_target(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['y'] = np.logical_xor(df['x1'], df['x2'])
+        df['y'] = np.logical_xor(df['x1'], df['x2']) 
         return df
     
-    # Method to save data
     def save(self, df: pd.DataFrame, out_path: os.path):
         df.to_csv(out_path, index=False)
-        logging.info(f'Data saved to {out_path}')
 
 
-# Main execution
 if __name__ == "__main__":
-    configure_logging()
     gen = XorSetGenerator()
     gen.create(len=256, save_path=TRAIN_PATH)
     gen.create(len=64, save_path=INFERENCE_PATH, is_labeled=False)
