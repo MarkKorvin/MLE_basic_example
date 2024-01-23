defc55df4594789f65c80c5287590df3ac2eb4a2
abajour <abajour0@gmail.com>
2024-01-24 00:11:38 +0100

    edited training and inference

--- inference/run.py
+++ inference/run.py
@@ -7,13 +7,14 @@ import argparse
 import json
 import logging
 import os
-import pickle
 import sys
 from datetime import datetime
 from typing import List
 
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+import torch
+from torch import nn
+import torch.nn.functional as F
 
 # Adds the root directory to system path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -47,19 +48,19 @@ def get_latest_model_path() -> str:
     latest = None
     for (dirpath, dirnames, filenames) in os.walk(MODEL_DIR):
         for filename in filenames:
-            if not latest or datetime.strptime(latest, conf['general']['datetime_format'] + '.pickle') < \
-                    datetime.strptime(filename, conf['general']['datetime_format'] + '.pickle'):
+            if not latest or datetime.strptime(latest, conf['general']['datetime_format'] + '.pt') < \
+                    datetime.strptime(filename, conf['general']['datetime_format'] + '.pt'):
                 latest = filename
     return os.path.join(MODEL_DIR, latest)
 
 
-def get_model_by_path(path: str) -> DecisionTreeClassifier:
-    """Loads and returns the specified model"""
+def get_model_by_path(path: str, input_size: int, output_size: int) -> nn.Module:
+    """Loads and returns the specified PyTorch model"""
     try:
-        with open(path, 'rb') as f:
-            model = pickle.load(f)
-            logging.info(f'Path of the model: {path}')
-            return model
+        model = SimpleModel(input_size, output_size)
+        model.load_state_dict(torch.load(path))
+        logging.info(f'Path of the model: {path}')
+        return model
     except Exception as e:
         logging.error(f'An error occurred while loading the model: {e}')
         sys.exit(1)
@@ -75,10 +76,14 @@ def get_inference_data(path: str) -> pd.DataFrame:
         sys.exit(1)
 
 
-def predict_results(model: DecisionTreeClassifier, infer_data: pd.DataFrame) -> pd.DataFrame:
+def predict_results(model: nn.Module, infer_data: pd.DataFrame, input_size: int) -> pd.DataFrame:
     """Predict de results and join it with the infer_data"""
-    results = model.predict(infer_data)
-    infer_data['results'] = results
+    infer_data_tensor = torch.tensor(infer_data.values, dtype=torch.float32)
+    with torch.no_grad():
+        outputs = model(infer_data_tensor)
+        _, predicted = torch.max(outputs, 1)
+
+    infer_data['results'] = predicted.numpy()
     return infer_data
 
 
@@ -98,10 +103,10 @@ def main():
     configure_logging()
     args = parser.parse_args()
 
-    model = get_model_by_path(get_latest_model_path())
+    model = get_model_by_path(get_latest_model_path(), input_size=2, output_size=2)
     infer_file = args.infer_file
     infer_data = get_inference_data(os.path.join(DATA_DIR, infer_file))
-    results = predict_results(model, infer_data)
+    results = predict_results(model, infer_data, input_size=2)
     store_results(results, args.out_path)
 
     logging.info(f'Prediction results: {results}')
--- training/train.py
+++ training/train.py
@@ -5,19 +5,21 @@ This script prepares the data, runs the training, and saves the model.
 import argparse
 import os
 import sys
-import pickle
 import json
 import logging
 import pandas as pd
 import time
 from datetime import datetime
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 
 # Comment this lines if you have problems with MLFlow installation
-import mlflow
-mlflow.autolog()
+# import mlflow
+# mlflow.autolog()
 
 # Adds the root directory to system path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -71,9 +73,27 @@ class DataProcessor():
         return df
 
 
+class SimpleClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(SimpleClassifier, self).__init__()
+        self.layer1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.layer2 = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        x = self.softmax(x)
+        return x
+
+
 class Training():
-    def __init__(self) -> None:
-        self.model = DecisionTreeClassifier(random_state=conf['general']['random_state'])
+    def __init__(self, input_size, hidden_size, output_size) -> None:
+        self.model = SimpleClassifier(input_size, hidden_size, output_size)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=conf['train']['learning_rate'])
 
     def run_training(self, df: pd.DataFrame, out_path: str = None, test_size: float = 0.33) -> None:
         logging.info("Running training...")
@@ -87,19 +107,44 @@ class Training():
 
     def data_split(self, df: pd.DataFrame, test_size: float = 0.33) -> tuple:
         logging.info("Splitting data into training and test sets...")
-        return train_test_split(df[['x1','x2']], df['y'], test_size=test_size, 
+        return train_test_split(df[['x1', 'x2']], df['y'], test_size=test_size,
                                 random_state=conf['general']['random_state'])
-    
+
     def train(self, X_train: pd.DataFrame, y_train: pd.DataFrame) -> None:
         logging.info("Training the model...")
-        self.model.fit(X_train, y_train)
+
+        X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+        y_train_tensor = torch.tensor(y_train.values, dtype=torch.long)
+
+        train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+        train_loader = DataLoader(train_dataset, batch_size=conf['train']['batch_size'], shuffle=True)
+
+        for epoch in range(conf['train']['num_epochs']):
+            for i, (inputs, labels) in enumerate(train_loader):
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                if (i + 1) % conf['train']['print_every'] == 0:
+                    logging.info(f'Epoch [{epoch + 1}/{conf["train"]["num_epochs"]}], '
+                                 f'Step [{i + 1}/{len(train_loader)}], '
+                                 f'Loss: {loss.item():.4f}')
 
     def test(self, X_test: pd.DataFrame, y_test: pd.DataFrame) -> float:
         logging.info("Testing the model...")
-        y_pred = self.model.predict(X_test)
-        res = f1_score(y_test, y_pred)
-        logging.info(f"f1_score: {res}")
-        return res
+
+        X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+        y_test_tensor = torch.tensor(y_test.values, dtype=torch.long)
+
+        with torch.no_grad():
+            outputs = self.model(X_test_tensor)
+            _, predicted = torch.max(outputs, 1)
+            f1 = f1_score(y_test_tensor.numpy(), predicted.numpy(), average='weighted')
+            logging.info(f"F1 Score: {f1}")
+
+        return f1
 
     def save(self, path: str) -> None:
         logging.info("Saving the model...")
@@ -107,22 +152,23 @@ class Training():
             os.makedirs(MODEL_DIR)
 
         if not path:
-            path = os.path.join(MODEL_DIR, datetime.now().strftime(conf['general']['datetime_format']) + '.pickle')
+            path = os.path.join(MODEL_DIR, datetime.now().strftime(conf['general']['datetime_format']) + '.pt')
         else:
             path = os.path.join(MODEL_DIR, path)
 
-        with open(path, 'wb') as f:
-            pickle.dump(self.model, f)
+        torch.save(self.model.state_dict(), path)
 
+prod_model = None
 
 def main():
     configure_logging()
 
     data_proc = DataProcessor()
-    tr = Training()
+    tr = Training(input_size=2, hidden_size=conf['train']['hidden_size'], output_size=2)
 
     df = data_proc.prepare_data(max_rows=conf['train']['data_sample'])
-    tr.run_training(df, test_size=conf['train']['test_size'])
+    global prod_model
+    prod_model = tr.run_training(df, test_size=conf['train']['test_size'])
 
 
 if __name__ == "__main__":
